c6326b9087e442a94151f6fde5751321dd96f13c
WabElH4x0r <wabelgaming@gmail.com>
2021-05-04 22:32:47 +0200

    fixes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,7 +14,7 @@
                     <img class="w-12 p-2.5" src="/steam.svg"/>
                 </a>
             </div>
-            <img class="absolute left-0 transform translate-y-16 -z-10" src="/waves.svg"/>
+            <img class="absolute left-0 transform translate-y-16 -z-10 w-full" src="/waves.svg"/>
         </header>
         <section id="informations" class="container mx-auto md:px-48 mt-48 p-10 md:p-24">
             <div class="grid grid-cols-1 lg:grid-cols-2 gap-14">
@@ -27,7 +27,7 @@
                         I’m Brique au bob, an 18yo french developer.
                         Mainly, i’m a back-end developer but i’m really
                         interested in learning front-end and UI/UX Design. 
-                        I’m learning programmation since I was 14.
+                        I’m learning programing since I was 14.
                     </p>
                 </div>
                 <div>
@@ -35,63 +35,61 @@
                 </div>
             </div>
         </section>
-        <section id="projects" class="relative text-center">
-            <img class="absolute top-20 md:top-4 xl:-top-12 -z-10" src="/waves_projects_top.svg" />
-            <div class="py-24 pt-24">
-                <div class="bg-gradient-to-r from-blue-700 to-blue-500">
-                    <div class="font-normal text-md text-gray-400">#projects</div>
-                    <div class="font-bold text-2xl text-white mb-4">My projects</div>
-                    <div class="container mx-auto sm:px-2 md:px-16 lg:px-48 grid grid-cols-1 lg:grid-cols-2 gap-8 mt-12">
-                        <Project 
-                            logo="/gca.png"
-                            name="Game Creators Area"
-                            description="French community of mutual help about various domains of activity like development, level design, modeling, graphism..."
-                            :languages="[
-                                '/laravel.png',
-                                '/php.png',
-                                '/mysql.svg',
-                                '/git.png',
-                                '/nginx.png',
-                            ]" 
-                        />
-                        <Project 
-                            logo="/ro-bot.png"
-                            name="Ro-bot"
-                            description="Website with the goal to create a Discord bot with a very customizable system and a bot running on the user defined token."
-                            :languages="[
-                                '/laravel.png',
-                                '/php.png',
-                                '/mongodb.png',
-                                '/git.png',
-                                '/nginx.png',
-                            ]" 
-                        />
-                        <Project 
-                            logo="/wearevalorant.png"
-                            name="WeAreValorant"
-                            description="It was designed to bring all the players of the Valorant game in a same place. A news system, tournament system and players find system was created for this."
-                            :languages="[
-                                '/adonis.png',
-                                '/nodejs.png',
-                                '/mysql.svg',
-                                '/git.png',
-                                '/nginx.png',
-                            ]" 
-                        />
-                        <Project 
-                            logo="/bobadmintoolkit.png"
-                            name="BobAdminToolKit"
-                            description="Garry’s Mod addon adding a lot of server administrators utilities like warns, logs, reports..."
-                            :languages="[
-                                '/sqlite.png',
-                                '/mysql.svg',
-                                '/lua.png',
-                            ]" 
-                        />
-                    </div>
+        <section id="projects" class="text-center">
+            <img class="relative -z-10 w-full transform translate-y-1" src="/waves_projects_top.svg" />
+            <div class="bg-gradient-to-r from-blue-700 to-blue-500">
+                <div class="font-normal text-md text-gray-400">#projects</div>
+                <div class="font-bold text-2xl text-white mb-4">My projects</div>
+                <div class="container mx-auto sm:px-2 md:px-16 lg:px-48 grid grid-cols-1 xl:grid-cols-2 gap-8 mt-12">
+                    <Project 
+                        logo="/gca.png"
+                        name="Game Creators Area"
+                        description="French community of mutual help about various domains of activity like development, level design, modeling, graphism..."
+                        :languages="[
+                            '/laravel.png',
+                            '/php.png',
+                            '/mysql.svg',
+                            '/git.png',
+                            '/nginx.png',
+                        ]" 
+                    />
+                    <Project 
+                        logo="/ro-bot.png"
+                        name="Ro-bot"
+                        description="Website with goal to create a Discord bot with a very customizable system and a bot running on user defined token."
+                        :languages="[
+                            '/laravel.png',
+                            '/php.png',
+                            '/mongodb.png',
+                            '/git.png',
+                            '/nginx.png',
+                        ]" 
+                    />
+                    <Project 
+                        logo="/wearevalorant.png"
+                        name="WeAreValorant"
+                        description="It was designed to bring all the players of Valorant's game in a same place. A news system, tournament system and players find system was created for this."
+                        :languages="[
+                            '/adonis.png',
+                            '/nodejs.png',
+                            '/mysql.svg',
+                            '/git.png',
+                            '/nginx.png',
+                        ]" 
+                    />
+                    <Project 
+                        logo="/bobadmintoolkit.png"
+                        name="BobAdminToolKit"
+                        description="Garry’s Mod addon adding a lot of server administrators utilities like warns, logs, reports..."
+                        :languages="[
+                            '/sqlite.png',
+                            '/mysql.svg',
+                            '/lua.png',
+                        ]" 
+                    />
                 </div>
             </div>
-            <img class="absolute transform -translate-y-28 -z-10" src="/waves_projects_bottom.svg" />
+            <img class="relative transform -translate-y-1 -z-10" src="/waves_projects_bottom.svg" />
         </section>
         <section id="services" class="container text-center mx-auto px-4 lg:px-48 mt-8 md:p-24">
             <div class="font-normal text-md text-gray-400">#services</div>
@@ -99,12 +97,12 @@
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-10 items-start">
                 <Service 
                     name="Back-End" 
-                    description="I can do the server-side of your project with PHP or NodeJS. Obviously, it includes the DBMS management." 
+                    description="I can do server-side of your project using PHP or NodeJS. Obviously, it includes DBMS management." 
                     icon="/icon_server.svg"
                 />
                 <Service 
                     name="Front-End" 
-                    description="I can do the client-side of your project with HTML/CSS/JS (also SaSS, NodeJS)." 
+                    description="I can do client-side of your project using HTML/CSS/JS (also SaSS, NodeJS)." 
                     icon="/icon_eye.svg"
                 />
                 <Service 
@@ -114,7 +112,7 @@
                 />
                 <Service 
                     name="Deployment" 
-                    description="I can deploy your project on your server after the development." 
+                    description="I can deploy your project on your server after development." 
                     icon="/icon_rocket.svg"
                 />
             </div>
